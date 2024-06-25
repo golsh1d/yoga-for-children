e590c058ee92fdff9b1705f73dc64bad80f25eb1
golshid <golsheedd@gmail.com>
2024-06-25 13:18:22 +0330

    develope responsive footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,19 +11,19 @@ module.exports = {
         "Morabba" : "Morabba",
         "MorabbaMedium" : "MorabbaMedium",
         "MorabbaBold" : "MorabbaBold",
-      }
-    },
-    screens : {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-    },
-    container : {
-      center : true,
-    },
-    letterSpacing: {
-      "tightest" : "-0.065em"
+      },
+      screens : {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+      },
+      container : {
+        center : true,
+      },
+      letterSpacing: {
+        "tightest" : "-0.065em"
+      },
     },
   },
   plugins: [],
