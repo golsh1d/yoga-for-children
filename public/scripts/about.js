56301b514f725273c1cf7211f382f5a358b6685a
golshid <golsheedd@gmail.com>
2024-06-26 11:13:34 +0330

    define functions for about page

--- public/scripts/about.js
+++ public/scripts/about.js
@@ -16,3 +16,96 @@ let footerChevronUpElem = document.querySelector('#jump-to-top')
 let headerElems = document.querySelectorAll('.header')
 let phoneNumElem = document.querySelector('#copy-to-clipboard')
 let copybadgeElem = document.querySelector('#copied')
+
+//changing the theme
+toggleThemeBtns.forEach(function (btn) {
+    btn.addEventListener("click" , () => {
+    if (localStorage.theme === "dark"){
+        document.documentElement.classList.remove("dark");
+        localStorage.theme = "light";
+    } else {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("theme" , "dark");
+    }
+})
+})
+
+// animation for hamburger icon
+function animationForIcon(event) {
+    let isChecked = event.target.checked
+    if(isChecked) {
+        console.log("change");
+        hamburgerSecondLine.style.display = 'none'
+        hamburgerFirstLine.style.transform = 'rotate(45deg) translateY(3.5px) translateX(4px)'
+        hamburgerFirstLine.style.transition = 'transform .3s ease'
+        hamburgerThirdLine.style.transform = 'rotate(-45deg) translateY(0.5px) translateX(0.5px)'
+        hamburgerThirdLine.style.transition = 'transform .3s ease'
+     }else{
+        console.log("unchange");
+        hamburgerSecondLine.style.display = 'block'
+        hamburgerFirstLine.style.transform = 'rotate(0deg) translateY(0px) translateX(0px)'
+        hamburgerFirstLine.style.transition = 'transform .3s ease'
+        hamburgerThirdLine.style.transform = 'rotate(0deg) translateY(0px) translateX(0px)'
+        hamburgerThirdLine.style.transition = 'transform .3s ease'
+     }
+}
+
+// changing display for side nav sub menu
+let isUp = true
+function changeSubmenuDisplay () {
+    if (isUp) {
+        chevronUp.style.transform = 'rotate(180deg)'
+        chevronUp.style.transition = 'transform .2s ease'
+        liSub.style.display = 'block'
+        liSub.style.transition = 'all 2s ease'
+        isUp = false
+    } else {
+        chevronUp.style.transform = 'rotate(0deg)'
+        chevronUp.style.transition = 'transform .2s ease'
+        liSub.style.display = 'none'
+        liSub.style.transition = 'all 2s ease'
+        isUp = true
+    }
+}
+
+// move side nav
+let isRight = true
+function moveSideNav() {
+    if (isRight) {
+        sideNav.style.transform = 'translateX(0px)'
+        sideNav.style.transition = 'transform .5s ease'
+        isRight = false
+        overlayElem.style.display = 'block'
+    } else {
+        sideNav.style.transform = 'translateX(256px)'
+        sideNav.style.transition = 'transform .5s ease'
+        isRight = true
+        overlayElem.style.display = 'none'
+    }
+}
+
+// move shopping card
+function moveShoppingCardToRight() {
+    sideShoppingCard.style.transform = 'translateX(0px)'
+    sideShoppingCard.style.transition = 'transform .5s ease'
+    overlayElem.style.display = 'block'
+}
+
+function moveShoppingCardToLeft() {
+    sideShoppingCard.style.transform = 'translateX(-256px)'
+    sideShoppingCard.style.transition = 'transform .5s ease' 
+    overlayElem.style.display = 'none'
+}
+
+// copy phone number to clipboard
+function copyToClipboard() {
+    let clipBoardText = '09122600520'
+
+    if (window.navigator.clipboard) {
+        window.navigator.clipboard.writeText(clipBoardText)
+        copybadgeElem.style.display = 'block'
+        setTimeout(() => {
+            copybadgeElem.style.display = 'none'
+        } , 700)
+    }
+}
