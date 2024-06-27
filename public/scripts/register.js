5d94f5eee6355325221cb586c6fa0951daec3a59
golshid <golsheedd@gmail.com>
2024-06-27 12:32:28 +0330

    add js codes to show and hide sign up validation messages

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -3,6 +3,7 @@ let inputsElems = document.querySelectorAll('input')
 let signInSpan = document.querySelector('.sign-in-span')
 let signUpSpan = document.querySelector('.sign-up-span')
 let wrapper = document.querySelector('.wrapper')
+let messages = document.querySelectorAll('.message')
 
 // move lables
 inputsElems.forEach(input => {
@@ -40,6 +41,39 @@ function moveSignUpPage() {
     wrapper.style.transition = 'transform 1s ease'
 }
 
+// show messages
+function checkUserNameValidation() {
+    if (inputsElems[2].value === '' || inputsElems[2].value.length < 3 || inputsElems[2].value.length > 20) {
+        messages[2].style.display = 'block'
+        messages[2].style.transition = 'transform 0.5s ease'
+    }
+}
+
+function checkPassWordValidation() {
+    if (inputsElems[3].value === '' || inputsElems[3].value.length < 3 || inputsElems[3].value.length > 30) {
+        messages[3].style.display = 'block'
+        messages[3].style.transition = 'transform 0.5s ease'
+    }
+}
+
+// hide messages
+function hideMessages() {
+    inputsElems.forEach(input => {
+        input.addEventListener('focus', () => {
+            messages.forEach(message => {
+                if (message.dataset.id === input.dataset.id) {
+                    message.style.display = 'none'
+                    message.style.transition = 'transform 0.5s ease'
+                }
+            })
+        })
+    })
+}
+
+hideMessages()
+
 // events
 signInSpan.addEventListener('click', moveSignInPage)
 signUpSpan.addEventListener('click', moveSignUpPage)
+inputsElems[2].addEventListener('blur' , checkUserNameValidation)
+inputsElems[3].addEventListener('blur' , checkPassWordValidation)
