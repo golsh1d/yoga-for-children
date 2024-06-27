c045e0fe5a030607ab912b8e26e0fa86a78caa24
golshid <golsheedd@gmail.com>
2024-06-27 10:50:21 +0330

    add better js codes to move lables

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -2,33 +2,26 @@ let lableElems = document.querySelectorAll('label')
 let inputsElems = document.querySelectorAll('input')
 
 // move lables
-function moveUserNameLableUp() {
-    lableElems[0].style.transform = 'translateY(-30px)'
-    lableElems[0].style.transition = 'transform .5s ease'
-}
+inputsElems.forEach(input => {
+    input.addEventListener('focus' , () => {
+        lableElems.forEach(lable => {
+            if (input.dataset.id === lable.dataset.id) {
+                lable.style.transform = 'translateY(-30px)'
+                lable.style.transition = 'transform 0.5s ease'
+            }
+        })
+    })
+})
 
-function moveUserNameLabledown() {
-    if (inputsElems[0].value === '') {
-        lableElems[0].style.transform = 'translateY(0px)'
-        lableElems[0].style.transition = 'transform .5s ease'
-    }
-}
-
-function movePassLableUp() {
-    lableElems[1].style.transform = 'translateY(-30px)'
-    lableElems[1].style.transition = 'transform .5s ease'
-}
-
-function movePassLabledown() {
-    if (inputsElems[1].value === '') {
-        lableElems[1].style.transform = 'translateY(0px)'
-        lableElems[1].style.transition = 'transform .5s ease'
-    }
-}
-
-
-// events
-inputsElems[0].addEventListener('focus' , moveUserNameLableUp)
-inputsElems[0].addEventListener('blur' , moveUserNameLabledown)
-inputsElems[1].addEventListener('focus' , movePassLableUp)
-inputsElems[1].addEventListener('blur' , movePassLabledown)
+inputsElems.forEach(input => {
+    input.addEventListener('blur' , () => {
+        lableElems.forEach(lable => {
+            if (input.dataset.id === lable.dataset.id) {
+                if (input.value === '') {
+                    lable.style.transform = 'translateY(0px)'
+                    lable.style.transition = 'transform 0.5s ease'
+                }
+            }
+        })
+    })
+})
