9540b468d2aebd5f5dac259351fadbb480d51db0
golshid <golsheedd@gmail.com>
2024-06-30 11:18:20 +0330

    code responsive single card page

--- public/scripts/singleCard.js
+++ public/scripts/singleCard.js
@@ -1,3 +1,23 @@
+const toggleThemeBtns = document.querySelectorAll(".toggle-theme");
+let hamburgerIcon = document.querySelector('#hamburger')
+let hamburgerFirstLine = document.querySelector('#hamburger-first-line')
+let hamburgerSecondLine = document.querySelector('#hamburger-second-line')
+let hamburgerThirdLine = document.querySelector('#hamburger-third-line')
+let checkBoxInput = document.querySelector('#ham')
+let liWithSub = document.querySelector('.has-sub-menu')
+let liSub = document.querySelector('.sub-menu')
+let chevronUp = document.querySelector('.chevron-up')
+let sideNav = document.querySelector('.side-nav')
+let sideShoppingCard = document.querySelector('.side-shopping-card')
+let xMarkIcon = document.querySelector('.x-mark')
+let shoppingCardIcon = document.querySelector('.shopping-card')
+let overlayElem = document.querySelector('.overlay')
+let footerChevronUpElem = document.querySelector('#jump-to-top')
+let headerElems = document.querySelectorAll('.header')
+let phoneNumElem = document.querySelector('.copy-to-clipboard')
+let copybadgeElem = document.querySelector('.copied')
+let swiperWrapper = document.querySelector('.swiper-wrapper')
+let productWrapper = document.querySelector('.product-wrapper')
 let allCardsInfo = [
     {id : 1, src : "./img/logo.png", title : "کارت های یوگای کودک", price : "135,000"},
     {id : 2, src : "./img/logo.png", title : "کارت های یوگای کودک", price : "145,000"},
@@ -7,7 +27,118 @@ let allCardsInfo = [
     {id : 6, src : "./img/logo.png", title : "کارت های یوگای کودک", price : "185,000"},
     {id : 7, src : "./img/logo.png", title : "کارت های یوگای کودک", price : "195,000"},
 ]
+
+//changing the theme
+toggleThemeBtns.forEach(function (btn) {
+    btn.addEventListener("click" , () => {
+    if (localStorage.theme === "dark"){
+        document.documentElement.classList.remove("dark");
+        localStorage.theme = "light";
+    } else {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("theme" , "dark");
+    }
+})
+})
+
+// animation for hamburger icon
+function animationForIcon(event) {
+    let isChecked = event.target.checked
+    if(isChecked) {
+        console.log("change");
+        hamburgerSecondLine.style.display = 'none'
+        hamburgerFirstLine.style.transform = 'rotate(45deg) translateY(3.5px) translateX(4px)'
+        hamburgerFirstLine.style.transition = 'transform .3s ease'
+        hamburgerThirdLine.style.transform = 'rotate(-45deg) translateY(0.5px) translateX(0.5px)'
+        hamburgerThirdLine.style.transition = 'transform .3s ease'
+     }else{
+        console.log("unchange");
+        hamburgerSecondLine.style.display = 'block'
+        hamburgerFirstLine.style.transform = 'rotate(0deg) translateY(0px) translateX(0px)'
+        hamburgerFirstLine.style.transition = 'transform .3s ease'
+        hamburgerThirdLine.style.transform = 'rotate(0deg) translateY(0px) translateX(0px)'
+        hamburgerThirdLine.style.transition = 'transform .3s ease'
+     }
+}
+
+// changing display for side nav sub menu
+let isUp = true
+function changeSubmenuDisplay () {
+    if (isUp) {
+        chevronUp.style.transform = 'rotate(180deg)'
+        chevronUp.style.transition = 'transform .2s ease'
+        liSub.style.display = 'block'
+        liSub.style.transition = 'all 2s ease'
+        isUp = false
+    } else {
+        chevronUp.style.transform = 'rotate(0deg)'
+        chevronUp.style.transition = 'transform .2s ease'
+        liSub.style.display = 'none'
+        liSub.style.transition = 'all 2s ease'
+        isUp = true
+    }
+}
+
+// move side nav
+let isRight = true
+function moveSideNav() {
+    if (isRight) {
+        sideNav.style.transform = 'translateX(0px)'
+        sideNav.style.transition = 'transform .5s ease'
+        isRight = false
+        overlayElem.style.display = 'block'
+    } else {
+        sideNav.style.transform = 'translateX(256px)'
+        sideNav.style.transition = 'transform .5s ease'
+        isRight = true
+        overlayElem.style.display = 'none'
+    }
+}
+
+// move shopping card
+function moveShoppingCardToRight() {
+    sideShoppingCard.style.transform = 'translateX(0px)'
+    sideShoppingCard.style.transition = 'transform .5s ease'
+    overlayElem.style.display = 'block'
+}
+
+function moveShoppingCardToLeft() {
+    sideShoppingCard.style.transform = 'translateX(-256px)'
+    sideShoppingCard.style.transition = 'transform .5s ease' 
+    overlayElem.style.display = 'none'
+}
+
+// scroll to specific element
+function scrollToTop() {
+    headerElems.forEach( header => {
+        header.scrollIntoView({ behavior : 'smooth'})
+    })
+}
+
+// copy phone number to clipboard
+function copyToClipboard() {
+    let clipBoardText = '09122600520'
+
+    if (window.navigator.clipboard) {
+        window.navigator.clipboard.writeText(clipBoardText)
+        copybadgeElem.style.display = 'block'
+        setTimeout(() => {
+            copybadgeElem.style.display = 'none'
+        } , 700)
+    }
+}
+
+
 let locationSearch = location.search
 let searchParams = new URLSearchParams(locationSearch)
 let pageId = searchParams.get('id')
 console.log(pageId)
+
+// events
+checkBoxInput.addEventListener('change', animationForIcon)
+chevronUp.addEventListener('click', changeSubmenuDisplay)
+hamburgerIcon.addEventListener('click' , moveSideNav)
+shoppingCardIcon.addEventListener('click' , moveShoppingCardToRight)
+xMarkIcon.addEventListener('click' , moveShoppingCardToLeft)
+footerChevronUpElem.addEventListener('click' , scrollToTop)
+phoneNumElem.addEventListener('click' , copyToClipboard)
