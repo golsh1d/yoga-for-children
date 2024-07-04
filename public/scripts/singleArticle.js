f697c0412b6ca4aa60be92f34532d5416ec95da9
golshid <golsheedd@gmail.com>
2024-07-04 13:37:41 +0330

    complete single article page shopping card

--- public/scripts/singleArticle.js
+++ public/scripts/singleArticle.js
@@ -17,6 +17,9 @@ let headerElems = document.querySelectorAll('.header')
 let phoneNumElem = document.querySelector('#copy-to-clipboard')
 let copybadgeElem = document.querySelector('#copied')
 let blogWrapper = document.querySelector('#blog-wrapper')
+let headerShoppingCard = document.querySelector('.header-shopping-card')
+let headerShoppingCardPrice = document.querySelector('.header-shopping-card-price')
+let headerShoppingCardItemCount = document.querySelector('.header-shopping-card-item-count')
 let allblogsInfo = [
     {id : 1, src : "./img/blogimg.png" , title : "مقاله ی اول یوگای کودک" , author : "ندا تاری وردی" , 
     desc : 
@@ -210,6 +213,110 @@ function loadData() {
     })
 }
 
+// generate shopping card
+function shoppingCardGenerator() {
+    headerShoppingCard.innerHTML = ''
+    shoppingCardProductArray.forEach(obj => {
+        headerShoppingCard.insertAdjacentHTML(`beforeend`,
+            `<div class="flex gap-x-2.5 items-center py-5 border-b-[1px] border-gray-300 dark:border-white/10">
+                        <div>
+                            <img class="w-[120px] h-[120px]" src="${obj.src}" alt="">
+                        </div>
+                        <div class="w-[230px]">
+                            <div class="w-full flex justify-between text-base text-zinc-700 dark:text-white mb-[28px]">
+                            <p class="font-DanaMedium">${obj.title}</p>
+                            <svg onclick="removeItem(${obj.id})" class="mt-[2px] w-4 h-4 cursor-pointer hover:text-orange-300">
+                                <use xlink:href="#X-mark"></use>
+                            </svg>
+                            </div>
+                            <div class="flex items-end">
+                                <div class="flex items-center justify-evenly w-[90px] h-11 ml-5 border-[1px] border-gray-300 rounded-full
+                                font-DanaDemiBold text-orange-300">
+                                <div onclick="inVal(${obj.id})" class="hover:text-orange-400 transition-colors">
+                                    <svg class="w-4 h-4 cursor-pointer">
+                                        <use xlink:href="#plus"></use>
+                                    </svg>
+                                </div>
+                                <p data-id="${obj.id}" class="val text-xl tracking-tighter">${obj.val}</p>
+                                <div onclick="decVal(${obj.id})" class="hover:text-orange-400 transition-colors">
+                                    <svg class="w-4 h-4 cursor-pointer">
+                                        <use xlink:href="#minus"></use>
+                                    </svg>
+                                </div>
+                            </div>
+                            <p class="font-Dana text-sm text-zinc-700 dark:text-white"><span class="font-DanaDemiBold text-xl">${obj.price}</span> تومان</p>
+                            </div>
+                        </div>
+                     </div>`
+        )
+    })
+    totalPrice()
+    totalItem()
+}
+
+// remove item
+function removeItem(id) {
+    let objIndex = shoppingCardProductArray.findIndex(obj => {
+        return obj.id === id
+    })
+    shoppingCardProductArray.splice(objIndex , 1)
+    shoppingCardGenerator()
+    setLocalStorage()
+}
+
+// calculate value
+function inVal(id) {
+    shoppingCardProductArray.forEach(obj => {
+        if (obj.id == id) {
+            ++obj.val 
+            totalPrice()
+        }
+    })
+    shoppingCardGenerator()
+    setLocalStorage()
+}
+
+function decVal(id) {
+    shoppingCardProductArray.forEach(obj => {
+        if (obj.id == id) {
+            if(obj.val > 1){
+                --obj.val 
+                totalPrice()
+            }
+        }
+    })
+    shoppingCardGenerator()
+    setLocalStorage()
+}
+
+function totalPrice() {
+    let sum = 0
+    headerShoppingCardPrice.innerHTML = '0'
+    shoppingCardProductArray.forEach(obj => {
+        sum += obj.price * obj.val
+        headerShoppingCardPrice.innerHTML = sum
+    })
+}
+
+// calculate total items
+function totalItem() {
+    let sum = shoppingCardProductArray.length
+    headerShoppingCardItemCount.innerHTML = sum
+}
+
+//local storage
+function setLocalStorage() {
+    localStorage.setItem('shoppingCardArray' , JSON.stringify(shoppingCardProductArray))
+}
+
+function loadHeaderShoppingCard() {
+    let localStorageArray = JSON.parse(localStorage.getItem('shoppingCardArray'))
+    if (localStorageArray) {
+        shoppingCardProductArray = localStorageArray
+        shoppingCardGenerator()
+    }
+}
+
 checkBoxInput.addEventListener('change', animationForIcon)
 chevronUp.addEventListener('click', changeSubmenuDisplay)
 hamburgerIcon.addEventListener('click' , moveSideNav)
@@ -218,3 +325,4 @@ xMarkIcon.addEventListener('click' , moveShoppingCardToLeft)
 footerChevronUpElem.addEventListener('click' , scrollToTop)
 phoneNumElem.addEventListener('click' , copyToClipboard)
 window.addEventListener('load' , loadData)
+window.addEventListener('load' , loadHeaderShoppingCard)
