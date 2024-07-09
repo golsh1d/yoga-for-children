90d2612a84f2e3f51a7de3c99663a0bc6c0ee1fb
golshid <golsheedd@gmail.com>
2024-07-09 10:20:41 +0330

    developed a responsive single board game page

--- public/scripts/singleBoardGame.js
+++ public/scripts/singleBoardGame.js
@@ -155,38 +155,84 @@ function loadData() {
             `<img class="w-full h-full" src="${obj.src4}" alt="">`)
             productWrapper.insertAdjacentHTML(`beforeend`,
             `<div class="w-full md:w-[60%] h-min text-zinc-700 dark:text-gray-100">
-                    <p class="font-MorabbaMedium text-lg md:text-xl lg:text-2xl xl:text-3xl mb-3 md:mb-5">${obj.title}</p>
-                    <div class="mb-5 md:mb-7">
-                        <p class="font-MorabbaMedium text-base md:text-lg lg:text-xl xl:text-2xl mb-1 md:mb-3 tracking-tight">مشخصات فنی</p>
-                        <ul class="space-y-1 md:space-y-3">
-                            <li class="flex items-center gap-x-2">
-                            <div class="w-2 h-2 bg-zinc-700 dark:bg-gray-100 rounded-full"></div>
-                            <p class="font-DanaMedium text-sm md:text-base lg:text-lg xl:text-xl tracking-tighter">ابعاد بوردگیم : ${obj.size} سانتی متر</p>
-                            </li>
-                            <li class="flex items-center gap-x-2">
-                            <div class="w-2 h-2 bg-zinc-700 dark:bg-gray-100 rounded-full"></div>
-                            <p class="font-DanaMedium text-sm md:text-base lg:text-lg xl:text-xl tracking-tighter">تعداد بوردگیم : ${obj.count} عدد</p>
-                            </li>
-                            <li class="flex items-center gap-x-2">
-                            <div class="w-2 h-2 bg-zinc-700 dark:bg-gray-100 rounded-full"></div>
-                            <p class="font-DanaMedium text-sm md:text-base lg:text-lg xl:text-xl tracking-tighter">جنس  بوردگیم : ${obj.material}</p>
-                            </li>
-                            <li class="flex items-center gap-x-2">
-                            <div class="w-2 h-2 bg-zinc-700 dark:bg-gray-100 rounded-full"></div>
-                            <p class="font-DanaMedium text-sm md:text-base lg:text-lg xl:text-xl tracking-tighter">وزن بسته : ${obj.weight} گرم</p>
-                            </li>
-                            <li class="flex items-center gap-x-2">
-                            <div class="w-2 h-2 bg-zinc-700 dark:bg-gray-100 rounded-full"></div>
-                            <p class="font-DanaMedium text-sm md:text-base lg:text-lg xl:text-xl tracking-tighter">هدف بازی : آموزشی</p>
-                            </li>
-                            <li class="flex items-center gap-x-2">
-                            <div class="w-2 h-2 bg-zinc-700 dark:bg-gray-100 rounded-full"></div>
-                            <p class="font-DanaMedium text-sm md:text-base lg:text-lg xl:text-xl tracking-tighter">رده سنی : مناسب برای کودکان</p>
-                            </li>
-                        </ul>
+                    <p class="font-MorabbaBold text-zinc-700 dark:text-gray-100 text-lg md:text-xl lg:text-2xl xl:text-3xl mb-6">${obj.title}</p>
+                    <div class="mb-8">
+                        <div class="flex items-center gap-x-2 mb-5">
+                        <svg class="w-10 sm:w-11 h-10 sm:h-11 text-orange-300">
+                            <use xlink:href="#exclamation"></use>
+                        </svg>
+                        <p class="font-MorabbaBold text-zinc-700 dark:text-gray-100 text-base md:text-lg xl:text-xl tracking-tight">مشخصات فنی</p>
+                        </div>
+                        <div class="grid grid-cols-2 lg:grid-cols-3 gap-6">
+                            <div class="flex flex-col md:flex-row text-center md:text-right items-center justify-center sm:justify-start gap-x-3 gap-y-2.5 bg-white/90 dark:bg-zinc-700 pt-4 pb-3.5 sm:py-3 px-4 rounded-xl
+                            font-Dana">
+                            <svg class="w-10 sm:w-11 h-10 sm:h-11 text-orange-300">
+                                <use xlink:href="#document"></use>
+                            </svg>
+                            <div class="space-y-0.5 sm:space-y-1">
+                                <p class="block font-DanaMedium text-sm sm:text-base text-zinc-700 dark:text-gray-100">ابعاد</p>
+                                <p class="block text-sm opacity-70 text-zinc-700 dark:text-gray-100">${obj.size} cm</p>
+                            </div>
+                            </div>
+                            <div class="flex flex-col md:flex-row text-center md:text-right items-center justify-center sm:justify-start gap-x-3 gap-y-2.5 bg-white/90 dark:bg-zinc-700 pt-4 pb-3.5 sm:py-3 px-4 rounded-xl
+                            font-Dana">
+                            <svg class="w-10 sm:w-11 h-10 sm:h-11 text-orange-300">
+                                <use xlink:href="#documents"></use>
+                            </svg>
+                            <div class="space-y-0.5 sm:space-y-1">
+                                <p class="block font-DanaMedium text-sm sm:text-base text-zinc-700 dark:text-gray-100">تعداد</p>
+                                <p class="block text-sm opacity-70 text-zinc-700 dark:text-gray-100">${obj.count} عدد</p>
+                            </div>
+                            </div>
+                            <div class="flex flex-col md:flex-row text-center md:text-right items-center justify-center sm:justify-start gap-x-3 gap-y-2.5 bg-white/90 dark:bg-zinc-700 pt-4 pb-3.5 sm:py-3 px-4 rounded-xl
+                            font-Dana">
+                            <svg class="w-10 sm:w-11 h-10 sm:h-11 text-orange-300">
+                                <use xlink:href="#material"></use>
+                            </svg>
+                            <div class="space-y-0.5 sm:space-y-1">
+                                <p class="block font-DanaMedium text-sm sm:text-base text-zinc-700 dark:text-gray-100">جنس</p>
+                                <p class="block text-sm opacity-70 text-zinc-700 dark:text-gray-100">${obj.material}</p>
+                            </div>
+                            </div>
+                            <div class="flex flex-col md:flex-row text-center md:text-right items-center justify-center sm:justify-start gap-x-3 gap-y-2.5 bg-white/90 dark:bg-zinc-700 pt-4 pb-3.5 sm:py-3 px-4 rounded-xl
+                            font-Dana">
+                            <svg class="w-10 sm:w-11 h-10 sm:h-11 text-orange-300">
+                                <use xlink:href="#scale"></use>
+                            </svg>
+                            <div class="space-y-0.5 sm:space-y-1">
+                                <p class="block font-DanaMedium text-sm sm:text-base text-zinc-700 dark:text-gray-100">وزن</p>
+                                <p class="block text-sm opacity-70 text-zinc-700 dark:text-gray-100">${obj.weight} گرم</p>
+                            </div>
+                            </div>
+                            <div class="flex flex-col md:flex-row text-center md:text-right items-center justify-center sm:justify-start gap-x-3 gap-y-2.5 bg-white/90 dark:bg-zinc-700 pt-4 pb-3.5 sm:py-3 px-4 rounded-xl
+                            font-Dana">
+                            <svg class="w-10 sm:w-11 h-10 sm:h-11 text-orange-300">
+                                <use xlink:href="#users"></use>
+                            </svg>
+                            <div class="space-y-0.5 sm:space-y-1">
+                                <p class="block font-DanaMedium text-sm sm:text-base text-zinc-700 dark:text-gray-100">رده ی سنی</p>
+                                <p class="block text-sm opacity-70 text-zinc-700 dark:text-gray-100"> برای کودکان</p>
+                            </div>
+                            </div>
+                            <div class="flex flex-col md:flex-row text-center md:text-right items-center justify-center sm:justify-start gap-x-3 gap-y-2.5 bg-white/90 dark:bg-zinc-700 pt-4 pb-3.5 sm:py-3 px-4 rounded-xl
+                            font-Dana">
+                            <svg class="w-10 sm:w-11 h-10 sm:h-11 text-orange-300">
+                                <use xlink:href="#question"></use>
+                            </svg>
+                            <div class="space-y-0.5 sm:space-y-1">
+                                <p class="block font-DanaMedium text-sm sm:text-base text-zinc-700 dark:text-gray-100">هدف</p>
+                                <p class="block text-sm opacity-70 text-zinc-700 dark:text-gray-100">آموزشی</p>
+                            </div>
+                            </div>
+                        </div>
                     </div>
-                    <p class="font-MorabbaMedium text-base md:text-lg lg:text-xl xl:text-2xl mb-1 md:mb-3 tracking-tight">توضیحات</p>
-                    <p class="font-DanaMedium text-sm md:text-base lg:text-lg xl:text-xl tracking-tight text-justify mb-3 md:mb-5">${obj.desc}</p>
+                    <div class="flex items-center gap-x-2 mb-5">
+                        <svg class="w-10 sm:w-11 h-10 sm:h-11 text-orange-300">
+                            <use xlink:href="#desc"></use>
+                        </svg>
+                        <p class="font-MorabbaBold text-zinc-700 dark:text-gray-100 text-base md:text-lg xl:text-xl">توضیحات</p>
+                    </div>
+                    <p class="font-DanaMedium text-sm sm:text-base text-zinc-700 dark:text-gray-100 text-justify mb-8">${obj.desc}</p>
                     <div class="flex items-center justify-between">
                         <div>
                             <p class="font-Dana text-sm md:text-base lg:text-lg xl:text-xl text-lime-900 dark:text-lime-100"><span class="text-lg md:text-xl lg:text-2xl xl:text-3xl">${obj.price}</span>تومان</p>
