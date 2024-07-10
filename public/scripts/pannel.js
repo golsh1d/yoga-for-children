9666d806bd81e470e1e04323d5966b74ff3e22f7
golshid <golsheedd@gmail.com>
2024-07-10 15:59:43 +0330

    add darkmode feature to pannel page

--- public/scripts/pannel.js
+++ public/scripts/pannel.js
@@ -1,7 +1,21 @@
+const toggleThemeBtns = document.querySelectorAll(".toggle-theme");
 let pannelIcon = document.querySelector('.pannel-icon')
 let sidePannel = document.querySelector('.side-pannel')
 let overlay = document.querySelector('.overlay')
 let closeElem = document.querySelector('.x-mark')
+
+//changing the theme
+toggleThemeBtns.forEach(function (btn) {
+    btn.addEventListener("click" , () => {
+    if (localStorage.theme === "dark"){
+        document.documentElement.classList.remove("dark");
+        localStorage.theme = "light";
+    } else {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("theme" , "dark");
+    }
+})
+})
 
 function showSidePannel() {
     sidePannel.style.transform = 'translateX(0px)'
