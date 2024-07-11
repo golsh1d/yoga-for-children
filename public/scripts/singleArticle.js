8beab51ce21ed2858fcc6ccfc7a263f3e8823d92
golshid <golsheedd@gmail.com>
2024-07-11 14:01:18 +0330

    add pannel code to single article page

--- public/scripts/singleArticle.js
+++ public/scripts/singleArticle.js
@@ -22,6 +22,11 @@ let headerShoppingCardPrice = document.querySelector('.header-shopping-card-pric
 let headerShoppingCardItemCount = document.querySelector('.header-shopping-card-item-count')
 let sideShoppingCardBody = document.querySelector('.side-shopping-card-body')
 let sideShoppingCardPrice = document.querySelector('.side-shopping-card-price')
+let pannelIcon = document.querySelector('.pannel-icon')
+let pannelContainer = document.querySelector('.pannel-container')
+let pannelSubIcon = document.querySelector('.pannel-sub-icon')
+let pannelSubMenu = document.querySelector('.pannel-sub-menu')
+let pannelSubli = document.querySelector('.has-pannel-sub')
 let allblogsInfo = [
     {id : 1, src : "./img/blogimg.png" , title : "مقاله ی اول یوگای کودک" , author : "ندا تاری وردی" , 
     desc : 
@@ -362,6 +367,38 @@ function loadHeaderShoppingCard() {
     }
 }
 
+let isHide = true
+function showPannel() {
+    if (isHide) {
+        pannelContainer.style.display = 'block'
+        overlayElem.style.display = 'block'
+        isHide = false
+    } else {
+        pannelContainer.style.display = 'none'
+        overlayElem.style.display = 'none'
+        isHide = true
+    }
+}
+
+let isShown = true
+function showPannelSub() {
+    if(isShown) {
+        pannelSubIcon.style.transform = 'rotate(180deg)'
+        pannelSubIcon.style.transition = 'transform 0.2s ease'
+        pannelSubli.style.marginBottom = '0px'
+        pannelSubMenu.style.display = 'block'
+        pannelSubMenu.style.transition = 'transform 0.2s ease'
+        isShown = false
+    } else {
+        pannelSubIcon.style.transform = 'rotate(0deg)'
+        pannelSubIcon.style.transition = 'transform 0.2s ease'
+        pannelSubli.style.marginBottom = '24px'
+        pannelSubMenu.style.display = 'none'
+        pannelSubMenu.style.transition = 'transform 0.2s ease'
+        isShown = true
+    }
+}
+
 checkBoxInput.addEventListener('change', animationForIcon)
 chevronUp.addEventListener('click', changeSubmenuDisplay)
 hamburgerIcon.addEventListener('click' , moveSideNav)
@@ -371,3 +408,5 @@ footerChevronUpElem.addEventListener('click' , scrollToTop)
 phoneNumElem.addEventListener('click' , copyToClipboard)
 window.addEventListener('load' , loadData)
 window.addEventListener('load' , loadHeaderShoppingCard)
+pannelIcon.addEventListener('click' , showPannel)
+pannelSubIcon.addEventListener('click' , showPannelSub)
