d6a449db4b75e29ddb708ed157c402ba98df8604
golshid <golsheedd@gmail.com>
2024-07-11 10:17:36 +0330

    dynamiced colors for pannel side nav

--- public/scripts/pannel.js
+++ public/scripts/pannel.js
@@ -4,7 +4,23 @@ let sidePannel = document.querySelector('.side-pannel')
 let overlay = document.querySelector('.overlay')
 let closeElem = document.querySelector('.x-mark')
 let myInfoLi = document.querySelector('.my-info-li')
+let myInfoLink = document.querySelector('.my-info-link')
+let myInfotext = document.querySelector('.my-info-text')
 let sideInfoLi = document.querySelector('.side-info-li')
+let sideInfoLink = document.querySelector('.side-info-link')
+let sideInfotext = document.querySelector('.side-info-text')
+let myCoursesLi = document.querySelector('.my-courses-li')
+let myCoursesLink = document.querySelector('.my-courses-link')
+let myCoursestext = document.querySelector('.my-courses-text')
+let sideCoursesLi = document.querySelector('.side-courses-li')
+let sideCoursesLink = document.querySelector('.side-courses-link')
+let sideCoursestext = document.querySelector('.side-courses-text')
+let myOrderLi = document.querySelector('.my-order-li')
+let myOrderLink = document.querySelector('.my-order-link')
+let myOrdertext = document.querySelector('.my-order-text')
+let sideOrderLi = document.querySelector('.side-order-li')
+let sideOrderLink = document.querySelector('.side-order-link')
+let sideOrdertext = document.querySelector('.side-order-text')
 let mainSection = document.querySelector('.main-section')
 
 //changing the theme
@@ -40,8 +56,28 @@ let pageId = searchParams.get('id')
 
 function loadData() {
     if (pageId == 3) {
+        myOrderLink.classList.add('text-gray-400')
+        myOrderLink.classList.add('dark:text-gray-100')
+        myOrdertext.classList.add('text-zinc-700')
+        myOrdertext.classList.add('dark:text-gray-100')
+        sideOrderLink.classList.add('text-gray-400')
+        sideOrderLink.classList.add('dark:text-gray-100')
+        sideOrdertext.classList.add('text-zinc-700')
+        sideOrdertext.classList.add('dark:text-gray-100')
         myInfoLi.classList.add('bg-orange-300')
+        myInfoLink.classList.add('text-white')
+        myInfotext.classList.add('text-white')
         sideInfoLi.classList.add('bg-orange-300')
+        sideInfoLink.classList.add('text-white')
+        sideInfotext.classList.add('text-white')
+        myCoursesLink.classList.add('text-gray-400')
+        myCoursesLink.classList.add('dark:text-gray-100')
+        myCoursestext.classList.add('text-zinc-700')
+        myCoursestext.classList.add('dark:text-gray-100')
+        sideCoursesLink.classList.add('text-gray-400')
+        sideCoursesLink.classList.add('dark:text-gray-100')
+        sideCoursestext.classList.add('text-zinc-700')
+        sideCoursestext.classList.add('dark:text-gray-100')
         mainSection.insertAdjacentHTML(`beforeend`,
             `<div class="w-full md:w-[70%] h-full p-5 bg-lime-50 rounded-2xl
         flex md:items-center">
@@ -89,6 +125,30 @@ function loadData() {
             </div>
         </div>`
         )
+    }
+    if (pageId == 2) {
+        myOrderLink.classList.add('text-gray-400')
+        myOrderLink.classList.add('dark:text-gray-100')
+        myOrdertext.classList.add('text-zinc-700')
+        myOrdertext.classList.add('dark:text-gray-100')
+        sideOrderLink.classList.add('text-gray-400')
+        sideOrderLink.classList.add('dark:text-gray-100')
+        sideOrdertext.classList.add('text-zinc-700')
+        sideOrdertext.classList.add('dark:text-gray-100')
+        myInfoLink.classList.add('text-gray-400')
+        myInfoLink.classList.add('dark:text-gray-100')
+        myInfotext.classList.add('text-zinc-700')
+        myInfotext.classList.add('dark:text-gray-100')
+        sideInfoLink.classList.add('text-gray-400')
+        sideInfoLink.classList.add('dark:text-gray-100')
+        sideInfotext.classList.add('text-zinc-700')
+        sideInfotext.classList.add('dark:text-gray-100')
+        myCoursesLi.classList.add('bg-orange-300')
+        myCoursesLink.classList.add('text-white')
+        myCoursestext.classList.add('text-white')
+        sideCoursesLi.classList.add('bg-orange-300')
+        sideCoursesLink.classList.add('text-white')
+        sideCoursestext.classList.add('text-white')
     }
 }
 
