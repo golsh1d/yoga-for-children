04c9cd0f12d0553fbde859e3d2fe2ed39152e712
golshid <golsheedd@gmail.com>
2024-07-11 13:31:42 +0330

    add pannel codes to cards page

--- public/scripts/cards.js
+++ public/scripts/cards.js
@@ -23,6 +23,11 @@ let headerShoppingCardPrice = document.querySelector('.header-shopping-card-pric
 let headerShoppingCardItemCount = document.querySelector('.header-shopping-card-item-count')
 let sideShoppingCardBody = document.querySelector('.side-shopping-card-body')
 let sideShoppingCardPrice = document.querySelector('.side-shopping-card-price')
+let pannelIcon = document.querySelector('.pannel-icon')
+let pannelContainer = document.querySelector('.pannel-container')
+let pannelSubIcon = document.querySelector('.pannel-sub-icon')
+let pannelSubMenu = document.querySelector('.pannel-sub-menu')
+let pannelSubli = document.querySelector('.has-pannel-sub')
 // data base
 let cardSliderInfo = [
     {id : 1, src : "./img/logo.png", title : "کارت های یوگای کودک", price : 135_000 , val : 1},
@@ -393,6 +398,38 @@ function loadHeaderShoppingCard() {
     }
 }
 
+let isHide = true
+function showPannel() {
+    if (isHide) {
+        pannelContainer.style.display = 'block'
+        overlayElem.style.display = 'block'
+        isHide = false
+    } else {
+        pannelContainer.style.display = 'none'
+        overlayElem.style.display = 'none'
+        isHide = true
+    }
+}
+
+let isShown = true
+function showPannelSub() {
+    if(isShown) {
+        pannelSubIcon.style.transform = 'rotate(180deg)'
+        pannelSubIcon.style.transition = 'transform 0.2s ease'
+        pannelSubli.style.marginBottom = '0px'
+        pannelSubMenu.style.display = 'block'
+        pannelSubMenu.style.transition = 'transform 0.2s ease'
+        isShown = false
+    } else {
+        pannelSubIcon.style.transform = 'rotate(0deg)'
+        pannelSubIcon.style.transition = 'transform 0.2s ease'
+        pannelSubli.style.marginBottom = '24px'
+        pannelSubMenu.style.display = 'none'
+        pannelSubMenu.style.transition = 'transform 0.2s ease'
+        isShown = true
+    }
+}
+
 // events
 checkBoxInput.addEventListener('change', animationForIcon)
 chevronUp.addEventListener('click', changeSubmenuDisplay)
@@ -402,3 +439,5 @@ xMarkIcon.addEventListener('click' , moveShoppingCardToLeft)
 footerChevronUpElem.addEventListener('click' , scrollToTop)
 phoneNumElem.addEventListener('click' , copyToClipboard)
 window.addEventListener('load' , loadHeaderShoppingCard)
+pannelIcon.addEventListener('click' , showPannel)
+pannelSubIcon.addEventListener('click' , showPannelSub)
