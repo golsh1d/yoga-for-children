1ae3f4e464c11b5451ce4fc1e5644fb9b33b6aff
golshid <golsheedd@gmail.com>
2024-10-31 17:55:35 +0330

    extract file from public

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["public/**/*.{html,js}"],
+  content: [
+    './*.{html,js}',
+    './scripts/*.js'
+  ],
   darkMode : "class",
   theme: {
     extend: {
